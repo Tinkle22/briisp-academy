0936dcac0aca49e332ed33e8cf9a8c0c9dade23c
Tinkle22 <104595797+Tinkle22@users.noreply.github.com>
2025-02-23 18:39:20 +0200

    Configure MySQL connection with SSL and explicit port

--- lib/db.ts
+++ lib/db.ts
@@ -2,20 +2,16 @@ import mysql from 'mysql2/promise';
 import fs from 'fs';
 import path from 'path';
 
-// const sslCertPath = process.env.DATABASE_SSL_CA;
-
-// if (!sslCertPath) {
-//   throw new Error("DATABASE_SSL_CA environment variable is not defined.");
-// }
-
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
+  port: parseInt(process.env.MYSQL_PORT || '20423'),
   database: process.env.MYSQL_DATABASE,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-// ssl: {
-//   ca: fs.readFileSync(path.resolve(process.cwd(), sslCertPath)),
-// },
+  ssl: {
+    ca: fs.readFileSync(path.resolve(process.cwd(), 'aiven.pem')),
+    rejectUnauthorized: true
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
